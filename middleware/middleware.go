59dc1eb8ed9213e8165931cdc8f4bfab32399b24
Italo Lelis de Vietro <italolelis@gmail.com>
2018-04-23 16:30:15 +0200

    Renamed pkg

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -1,18 +1,19 @@
-package http
+package middleware
 
 import (
 	"net/http"
 
 	"github.com/felixge/httpsnoop"
 	"github.com/hellofresh/stats-go/client"
+	"github.com/hellofresh/stats-go/context"
 	"github.com/hellofresh/stats-go/timer"
 )
 
-// NewStatsRequest creates a new middleware
-func NewStatsRequest(s client.Client) func(http.Handler) http.Handler {
+// New creates a new stats middleware
+func New(s client.Client) func(http.Handler) http.Handler {
 	return func(handler http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			r = r.WithContext(NewContext(r.Context(), s))
+			r = r.WithContext(context.New(r.Context(), s))
 
 			mt := httpsnoop.CaptureMetrics(handler, w, r)
 			t := timer.NewDuration(mt.Duration)

26e4c33ad4bca6972a034078ff8f2caacc3f85a7
Italo Lelis de Vietro <italolelis@gmail.com>
2018-04-23 16:30:04 +0200

    Added the context pkg

---
+++ context/context.go
@@ -0,0 +1,26 @@
+package context
+
+import (
+	"context"
+
+	"github.com/hellofresh/stats-go/client"
+)
+
+type statsKeyType int
+
+const statsKey statsKeyType = iota
+
+// New returns a context that has a stats Client
+func New(ctx context.Context, client client.Client) context.Context {
+	return context.WithValue(ctx, statsKey, client)
+}
+
+// WithContext returns a stats Client with as much context as possible
+func WithContext(ctx context.Context) client.Client {
+	ctxStats, ok := ctx.Value(statsKey).(client.Client)
+	if !ok {
+		return client.NewNoop(true)
+	}
+
+	return ctxStats
+}

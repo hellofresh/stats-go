998c8fb8368d61033fef9d278ffd340f2699ed92
Alexander Zhilyaev <azh@hellofresh.com>
2018-05-04 10:49:32 +0200

    make mock attributes private

--- client/prometheus_test.go
+++ client/prometheus_test.go
@@ -13,67 +13,67 @@ import (
 
 // Mock incrementer object
 type mockIncrementer struct {
-	IncrementMethodCalled  int
-	IncrementMethodMetrics []string
-	IncrementMethodLabels  []map[string]string
+	incrementMethodCalled  int
+	incrementMethodMetrics []string
+	incrementMethodLabels  []map[string]string
 
-	IncrementNMethodCalled  int
-	IncrementNMethodMetrics []string
-	IncrementNMethodLabels  []map[string]string
+	incrementNMethodCalled  int
+	incrementNMethodMetrics []string
+	incrementNMethodLabels  []map[string]string
 
-	IncrementALlMethodCalled  int
-	IncrementALlNMethodCalled int
+	incrementALlMethodCalled  int
+	incrementALlNMethodCalled int
 }
 
 func (i *mockIncrementer) Increment(metric string, labels ...map[string]string) {
-	i.IncrementMethodCalled++
-	i.IncrementMethodMetrics = append(i.IncrementMethodMetrics, metric)
+	i.incrementMethodCalled++
+	i.incrementMethodMetrics = append(i.incrementMethodMetrics, metric)
 	if labels[0] != nil {
-		i.IncrementMethodLabels = append(i.IncrementMethodLabels, labels[0])
+		i.incrementMethodLabels = append(i.incrementMethodLabels, labels[0])
 	}
 }
 
 func (i *mockIncrementer) IncrementN(metric string, n int, labels ...map[string]string) {
-	i.IncrementNMethodCalled++
-	i.IncrementNMethodMetrics = append(i.IncrementNMethodMetrics, metric)
+	i.incrementNMethodCalled++
+	i.incrementNMethodMetrics = append(i.incrementNMethodMetrics, metric)
 	if labels[0] != nil {
-		i.IncrementNMethodLabels = append(i.IncrementNMethodLabels, labels[0])
+		i.incrementNMethodLabels = append(i.incrementNMethodLabels, labels[0])
 	}
 }
 
 func (i *mockIncrementer) IncrementAll(b bucket.Bucket) {
-	i.IncrementALlMethodCalled++
+	i.incrementALlMethodCalled++
 }
 
 func (i *mockIncrementer) IncrementAllN(b bucket.Bucket, n int) {
-	i.IncrementALlNMethodCalled++
+	i.incrementALlNMethodCalled++
 }
 
 // Mock IncrementFactory object
 type mockIncrementerFactory struct {
 	inc *mockIncrementer
 
-	CreateMethodCalled int
+	createMethodCalled int
 }
 
 func newMockIncrementerFactory() *mockIncrementerFactory {
-	return &mockIncrementerFactory{CreateMethodCalled: 0}
+	return &mockIncrementerFactory{createMethodCalled: 0}
 }
 
 func (m *mockIncrementerFactory) Create() incrementer.Incrementer {
-	m.CreateMethodCalled++
+	m.createMethodCalled++
 	if m.inc == nil {
 		m.inc = &mockIncrementer{
-			IncrementMethodCalled:  0,
-			IncrementMethodMetrics: []string{},
-			IncrementMethodLabels:  []map[string]string{},
+			incrementMethodCalled:  0,
+			incrementMethodMetrics: []string{},
+			incrementMethodLabels:  []map[string]string{},
 
-			IncrementNMethodCalled:  0,
-			IncrementNMethodMetrics: []string{},
-			IncrementNMethodLabels:  []map[string]string{},
+			incrementNMethodCalled:  0,
+			incrementNMethodMetrics: []string{},
+			incrementNMethodLabels:  []map[string]string{},
 
-			IncrementALlMethodCalled:  0,
-			IncrementALlNMethodCalled: 0,
+			incrementALlMethodCalled:  0,
+			incrementALlNMethodCalled: 0,
 		}
 	}
 	return m.inc
@@ -81,43 +81,43 @@ func (m *mockIncrementerFactory) Create() incrementer.Incrementer {
 
 // Mock state object
 type mockState struct {
-	SetMethodCalled  int
-	SetMethodNumbers []int
-	SetMethodMetrics []string
-	SetMethodLabels  []map[string]string
+	setMethodCalled  int
+	setMethodNumbers []int
+	setMethodMetrics []string
+	setMethodLabels  []map[string]string
 }
 
 func (s *mockState) Set(metric string, n int, labels ...map[string]string) {
-	s.SetMethodCalled++
-	s.SetMethodMetrics = append(s.SetMethodMetrics, metric)
-	s.SetMethodNumbers = append(s.SetMethodNumbers, n)
+	s.setMethodCalled++
+	s.setMethodMetrics = append(s.setMethodMetrics, metric)
+	s.setMethodNumbers = append(s.setMethodNumbers, n)
 	if labels[0] != nil {
-		s.SetMethodLabels = append(s.SetMethodLabels, labels[0])
+		s.setMethodLabels = append(s.setMethodLabels, labels[0])
 	}
 }
 
 // Mock StateFactory object
 type mockStateFactory struct {
-	S *mockState
+	s *mockState
 
-	CreateMethodCalled int
+	createMethodCalled int
 }
 
 func newMockStateFactory() *mockStateFactory {
-	return &mockStateFactory{CreateMethodCalled: 0}
+	return &mockStateFactory{createMethodCalled: 0}
 }
 
 func (m *mockStateFactory) Create() state.State {
-	m.CreateMethodCalled++
-	if m.S == nil {
-		m.S = &mockState{
-			SetMethodCalled:  0,
-			SetMethodMetrics: []string{},
-			SetMethodNumbers: []int{},
-			SetMethodLabels:  []map[string]string{},
+	m.createMethodCalled++
+	if m.s == nil {
+		m.s = &mockState{
+			setMethodCalled:  0,
+			setMethodMetrics: []string{},
+			setMethodNumbers: []int{},
+			setMethodLabels:  []map[string]string{},
 		}
 	}
-	return m.S
+	return m.s
 }
 
 // Tests block begin
@@ -151,11 +151,11 @@ func TestPrometheusClient_TrackMetric(t *testing.T) {
 	p := NewPrometheus("namespace", m, newMockStateFactory())
 	p.TrackMetric("section", bucket.NewMetricOperation("foo", "bar", "baz"))
 
-	assert.Equal(t, 2, m.CreateMethodCalled)
+	assert.Equal(t, 2, m.createMethodCalled)
 
-	assert.Equal(t, 2, m.inc.IncrementMethodCalled)
-	assert.Equal(t, []string{"section_foo_bar_baz", "total_section"}, m.inc.IncrementMethodMetrics)
-	assert.Equal(t, []map[string]string{}, m.inc.IncrementMethodLabels)
+	assert.Equal(t, 2, m.inc.incrementMethodCalled)
+	assert.Equal(t, []string{"section_foo_bar_baz", "total_section"}, m.inc.incrementMethodMetrics)
+	assert.Equal(t, []map[string]string{}, m.inc.incrementMethodLabels)
 }
 
 func TestPrometheusClient_TrackMetricIncrementsAlreadyExists(t *testing.T) {
@@ -164,16 +164,16 @@ func TestPrometheusClient_TrackMetricIncrementsAlreadyExists(t *testing.T) {
 	p.TrackMetric("section", bucket.NewMetricOperation("foo", "bar", "baz"))
 	p.TrackMetric("section", bucket.NewMetricOperation("foo", "bar", "baz"))
 
-	assert.Equal(t, 2, m.CreateMethodCalled)
+	assert.Equal(t, 2, m.createMethodCalled)
 
-	assert.Equal(t, 4, m.inc.IncrementMethodCalled)
-	assert.Equal(t, []string{"section_foo_bar_baz", "total_section", "section_foo_bar_baz", "total_section"}, m.inc.IncrementMethodMetrics)
-	assert.Equal(t, []map[string]string{}, m.inc.IncrementMethodLabels)
+	assert.Equal(t, 4, m.inc.incrementMethodCalled)
+	assert.Equal(t, []string{"section_foo_bar_baz", "total_section", "section_foo_bar_baz", "total_section"}, m.inc.incrementMethodMetrics)
+	assert.Equal(t, []map[string]string{}, m.inc.incrementMethodLabels)
 
-	assert.Equal(t, 0, m.inc.IncrementNMethodCalled)
+	assert.Equal(t, 0, m.inc.incrementNMethodCalled)
 
-	assert.Equal(t, 0, m.inc.IncrementALlMethodCalled)
-	assert.Equal(t, 0, m.inc.IncrementALlNMethodCalled)
+	assert.Equal(t, 0, m.inc.incrementALlMethodCalled)
+	assert.Equal(t, 0, m.inc.incrementALlNMethodCalled)
 }
 
 func TestPrometheusClient_TrackMetricN(t *testing.T) {
@@ -181,16 +181,16 @@ func TestPrometheusClient_TrackMetricN(t *testing.T) {
 	p := NewPrometheus("namespace", m, newMockStateFactory())
 	p.TrackMetricN("section", bucket.NewMetricOperation("foo", "bar", "baz"), 999)
 
-	assert.Equal(t, 2, m.CreateMethodCalled)
+	assert.Equal(t, 2, m.createMethodCalled)
 
-	assert.Equal(t, 0, m.inc.IncrementMethodCalled)
+	assert.Equal(t, 0, m.inc.incrementMethodCalled)
 
-	assert.Equal(t, 2, m.inc.IncrementNMethodCalled)
-	assert.Equal(t, []string{"section_foo_bar_baz", "total_section"}, m.inc.IncrementNMethodMetrics)
-	assert.Equal(t, []map[string]string{}, m.inc.IncrementNMethodLabels)
+	assert.Equal(t, 2, m.inc.incrementNMethodCalled)
+	assert.Equal(t, []string{"section_foo_bar_baz", "total_section"}, m.inc.incrementNMethodMetrics)
+	assert.Equal(t, []map[string]string{}, m.inc.incrementNMethodLabels)
 
-	assert.Equal(t, 0, m.inc.IncrementALlMethodCalled)
-	assert.Equal(t, 0, m.inc.IncrementALlNMethodCalled)
+	assert.Equal(t, 0, m.inc.incrementALlMethodCalled)
+	assert.Equal(t, 0, m.inc.incrementALlNMethodCalled)
 }
 
 func TestPrometheusClient_TrackState(t *testing.T) {
@@ -198,12 +198,12 @@ func TestPrometheusClient_TrackState(t *testing.T) {
 	s := newMockStateFactory()
 	p := NewPrometheus("namespace", m, s)
 	p.TrackState("section", bucket.NewMetricOperation("foo", "bar", "baz"), 888)
-	assert.Equal(t, 1, s.CreateMethodCalled)
+	assert.Equal(t, 1, s.createMethodCalled)
 
-	assert.Equal(t, 1, s.S.SetMethodCalled)
-	assert.Equal(t, []string{"section_foo_bar_baz"}, s.S.SetMethodMetrics)
-	assert.Equal(t, []int{888}, s.S.SetMethodNumbers)
-	assert.Equal(t, []map[string]string{}, s.S.SetMethodLabels)
+	assert.Equal(t, 1, s.s.setMethodCalled)
+	assert.Equal(t, []string{"section_foo_bar_baz"}, s.s.setMethodMetrics)
+	assert.Equal(t, []int{888}, s.s.setMethodNumbers)
+	assert.Equal(t, []map[string]string{}, s.s.setMethodLabels)
 }
 
 func TestPrometheusClient_TrackStateAlreadyExists(t *testing.T) {
@@ -212,12 +212,12 @@ func TestPrometheusClient_TrackStateAlreadyExists(t *testing.T) {
 	p := NewPrometheus("namespace", m, s)
 	p.TrackState("section", bucket.NewMetricOperation("foo", "bar", "baz"), 888)
 	p.TrackState("section", bucket.NewMetricOperation("foo", "bar", "baz"), 888)
-	assert.Equal(t, 1, s.CreateMethodCalled)
+	assert.Equal(t, 1, s.createMethodCalled)
 
-	assert.Equal(t, 2, s.S.SetMethodCalled)
-	assert.Equal(t, []string{"section_foo_bar_baz", "section_foo_bar_baz"}, s.S.SetMethodMetrics)
-	assert.Equal(t, []int{888, 888}, s.S.SetMethodNumbers)
-	assert.Equal(t, []map[string]string{}, s.S.SetMethodLabels)
+	assert.Equal(t, 2, s.s.setMethodCalled)
+	assert.Equal(t, []string{"section_foo_bar_baz", "section_foo_bar_baz"}, s.s.setMethodMetrics)
+	assert.Equal(t, []int{888, 888}, s.s.setMethodNumbers)
+	assert.Equal(t, []map[string]string{}, s.s.setMethodLabels)
 }
 
 func TestPrometheusClient_TrackOperation(t *testing.T) {
@@ -227,11 +227,11 @@ func TestPrometheusClient_TrackOperation(t *testing.T) {
 
 	p.TrackOperation("section", bucket.NewMetricOperation("foo", "bar", "baz"), nil, true)
 
-	assert.Equal(t, 2, m.CreateMethodCalled)
+	assert.Equal(t, 2, m.createMethodCalled)
 
-	assert.Equal(t, 2, m.inc.IncrementMethodCalled)
-	assert.Equal(t, []string{"section_foo_bar_baz", "total_section"}, m.inc.IncrementMethodMetrics)
-	assert.Equal(t, []map[string]string{{"success": "true"}, {"success": "true"}}, m.inc.IncrementMethodLabels)
+	assert.Equal(t, 2, m.inc.incrementMethodCalled)
+	assert.Equal(t, []string{"section_foo_bar_baz", "total_section"}, m.inc.incrementMethodMetrics)
+	assert.Equal(t, []map[string]string{{"success": "true"}, {"success": "true"}}, m.inc.incrementMethodLabels)
 }
 
 func TestPrometheusClient_TrackOperationWithTimer(t *testing.T) {
@@ -244,11 +244,11 @@ func TestPrometheusClient_TrackOperationWithTimer(t *testing.T) {
 
 	p.TrackOperation("section", bucket.NewMetricOperation("foo", "bar", "baz"), tt, true)
 
-	assert.Equal(t, 2, m.CreateMethodCalled)
+	assert.Equal(t, 2, m.createMethodCalled)
 
-	assert.Equal(t, 2, m.inc.IncrementMethodCalled)
-	assert.Equal(t, []string{"section_foo_bar_baz", "total_section"}, m.inc.IncrementMethodMetrics)
-	assert.Equal(t, []map[string]string{{"success": "true"}, {"success": "true"}}, m.inc.IncrementMethodLabels)
+	assert.Equal(t, 2, m.inc.incrementMethodCalled)
+	assert.Equal(t, []string{"section_foo_bar_baz", "total_section"}, m.inc.incrementMethodMetrics)
+	assert.Equal(t, []map[string]string{{"success": "true"}, {"success": "true"}}, m.inc.incrementMethodLabels)
 
 	assert.NotNil(t, p.histograms["section_foo_bar_baz"])
 	_, err := p.histograms["section_foo_bar_baz"].GetMetricWithLabelValues("true")
@@ -263,11 +263,11 @@ func TestPrometheusClient_TrackOperationAlreadyExists(t *testing.T) {
 	p.TrackOperation("section", bucket.NewMetricOperation("foo", "bar", "baz"), nil, true)
 	p.TrackOperation("section", bucket.NewMetricOperation("foo", "bar", "baz"), nil, false)
 
-	assert.Equal(t, 2, m.CreateMethodCalled)
+	assert.Equal(t, 2, m.createMethodCalled)
 
-	assert.Equal(t, 4, m.inc.IncrementMethodCalled)
-	assert.Equal(t, []string{"section_foo_bar_baz", "total_section", "section_foo_bar_baz", "total_section"}, m.inc.IncrementMethodMetrics)
-	assert.Equal(t, []map[string]string{{"success": "true"}, {"success": "true"}, {"success": "false"}, {"success": "false"}}, m.inc.IncrementMethodLabels)
+	assert.Equal(t, 4, m.inc.incrementMethodCalled)
+	assert.Equal(t, []string{"section_foo_bar_baz", "total_section", "section_foo_bar_baz", "total_section"}, m.inc.incrementMethodMetrics)
+	assert.Equal(t, []map[string]string{{"success": "true"}, {"success": "true"}, {"success": "false"}, {"success": "false"}}, m.inc.incrementMethodLabels)
 }
 
 func TestPrometheusClient_TrackOperationN(t *testing.T) {
@@ -277,11 +277,11 @@ func TestPrometheusClient_TrackOperationN(t *testing.T) {
 
 	p.TrackOperationN("section", bucket.NewMetricOperation("foo", "bar", "baz"), nil, 999, true)
 
-	assert.Equal(t, 2, m.CreateMethodCalled)
+	assert.Equal(t, 2, m.createMethodCalled)
 
-	assert.Equal(t, 2, m.inc.IncrementNMethodCalled)
-	assert.Equal(t, []string{"section_foo_bar_baz", "total_section"}, m.inc.IncrementNMethodMetrics)
-	assert.Equal(t, []map[string]string{{"success": "true"}, {"success": "true"}}, m.inc.IncrementNMethodLabels)
+	assert.Equal(t, 2, m.inc.incrementNMethodCalled)
+	assert.Equal(t, []string{"section_foo_bar_baz", "total_section"}, m.inc.incrementNMethodMetrics)
+	assert.Equal(t, []map[string]string{{"success": "true"}, {"success": "true"}}, m.inc.incrementNMethodLabels)
 }
 
 func TestPrometheusClient_TrackOperationNWithTimer(t *testing.T) {
@@ -294,10 +294,10 @@ func TestPrometheusClient_TrackOperationNWithTimer(t *testing.T) {
 
 	p.TrackOperationN("section", bucket.NewMetricOperation("foo", "bar", "baz"), tt, 999, true)
 
-	assert.Equal(t, 2, m.CreateMethodCalled)
-	assert.Equal(t, 2, m.inc.IncrementNMethodCalled)
-	assert.Equal(t, []string{"section_foo_bar_baz", "total_section"}, m.inc.IncrementNMethodMetrics)
-	assert.Equal(t, []map[string]string{{"success": "true"}, {"success": "true"}}, m.inc.IncrementNMethodLabels)
+	assert.Equal(t, 2, m.createMethodCalled)
+	assert.Equal(t, 2, m.inc.incrementNMethodCalled)
+	assert.Equal(t, []string{"section_foo_bar_baz", "total_section"}, m.inc.incrementNMethodMetrics)
+	assert.Equal(t, []map[string]string{{"success": "true"}, {"success": "true"}}, m.inc.incrementNMethodLabels)
 
 	assert.NotNil(t, p.histograms["section_foo_bar_baz"])
 	_, err := p.histograms["section_foo_bar_baz"].GetMetricWithLabelValues("true")
@@ -313,9 +313,9 @@ func TestPrometheusClient_TrackOperationNAlreadyExists(t *testing.T) {
 	p.TrackOperationN("section", bucket.NewMetricOperation("foo", "bar", "baz"), nil, 1, true)
 	p.TrackOperationN("section", bucket.NewMetricOperation("foo", "bar", "baz"), nil, 2, false)
 
-	assert.Equal(t, 2, m.CreateMethodCalled)
+	assert.Equal(t, 2, m.createMethodCalled)
 
-	assert.Equal(t, 4, m.inc.IncrementNMethodCalled)
-	assert.Equal(t, []string{"section_foo_bar_baz", "total_section", "section_foo_bar_baz", "total_section"}, m.inc.IncrementNMethodMetrics)
-	assert.Equal(t, []map[string]string{{"success": "true"}, {"success": "true"}, {"success": "false"}, {"success": "false"}}, m.inc.IncrementNMethodLabels)
+	assert.Equal(t, 4, m.inc.incrementNMethodCalled)
+	assert.Equal(t, []string{"section_foo_bar_baz", "total_section", "section_foo_bar_baz", "total_section"}, m.inc.incrementNMethodMetrics)
+	assert.Equal(t, []map[string]string{{"success": "true"}, {"success": "true"}, {"success": "false"}, {"success": "false"}}, m.inc.incrementNMethodLabels)
 }
